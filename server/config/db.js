a7c31c05c4fd15df561bacccfbd427036546ce5e
Sanjay R <sanpro2004.jay@gmail.com>
2025-09-24 11:22:34 +0530

    add ca certificate

--- server/config/db.js
+++ server/config/db.js
@@ -18,25 +18,34 @@
 
 
 
-
-
-
 const mysql = require('mysql2/promise');
+const fs = require('fs');
 require('dotenv').config();
 
 let db;
 
 if (process.env.DB_URL) {
-  // Use full connection string
-  db = mysql.createPool(process.env.DB_URL);
+  // Use full connection string + SSL
+  db = mysql.createPool({
+    uri: process.env.DB_URL,
+    ssl: {
+      ca: fs.readFileSync(process.env.DB_CA_CERT) // load CA cert
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
 } else {
-  // Use individual env variables
+  // Use individual env variables + SSL
   db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
+    ssl: {
+      ca: fs.readFileSync(process.env.DB_CA_CERT) // load CA cert
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
